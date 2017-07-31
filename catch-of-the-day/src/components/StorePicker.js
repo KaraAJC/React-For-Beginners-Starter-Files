2caadb2d1473c99122e457e3e60ed1b68215a16e
KaraAJC <KaraAJC@gmail.com>
2017-07-31 13:14:27 -0500

    completed up to lesson 3

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,16 @@
+// classes are used to create components.
+// components live inside their own ComponentName.js file
+// component files always start with import React from 'react'
+import React from 'react';
+
+
+class StorePicker extends React.Component {
+	// render is always a function component holds
+	render() {
+		return <p>Hello</p>
+	}
+}
+
+
+// component file always export tag
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+// this is an ES6 module!
+import React from 'react'; // uses package.json to pull in 'react' from dependencies to 'React'  variable
+
+// this is where the render method comes from
+import { render } from 'react-dom';
+// you always import your components to the main.js file like:
+
+// import ComponentName from './components/ComponentName'
+import StorePicker from './components/StorePicker'
+
+
+
+
+
+
+
+
+render(<StorePicker/>, document.querySelector('#main') )
+// render(TagName, element selector)
+
